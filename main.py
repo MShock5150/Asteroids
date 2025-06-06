38d30d4f09ea21cc35fe7bd4e86dcf9d8d763c70
MShock5150 <corleym5150@gmail.com>
2025-06-06 02:19:40 -0500

    Add in Player movement and Circle shape

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    Player.containers = (updatables, drawables)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
                  return
+
+        updatables.update(dt)
+        # Handle player input
         screen.fill("black")
+        
+        for sprite in drawables:
+            sprite.draw(screen)
+
         pygame.display.flip()
+
+        # Limit the frame rate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
